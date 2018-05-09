fe5ed6fbeb8a78029bc70ac39ecbbaf7d06aa054
Jonathan Pe <jonathanq@hotmail.com>
2018-05-08 22:10:59 -0700

    push latest

--- app/components/champion-table.js
+++ app/components/champion-table.js
@@ -1,20 +1,11 @@
 import Component from '@ember/component';
 import config from 'lolsight/config/environment';
-import { inject as service } from '@ember/service';
 
 export default Component.extend({
     champion: null,
     apiVersion: config.riotApiVersion,
 
-    router: service(),
-
     didInsertElement() {
-        //Split URL and find just the champ ID
-        const champId = window.location.pathname.split('/')[2];
-        fetch(`http://localhost:3001/champions/${champId}`).then(response => {
-            response.json().then(champion => {
-                this.set('champion', champion);
-            })
-        });
+        this.set('champion', this.get('model.champion'));
     },
 });
--- app/routes/champions/details.js
+++ app/routes/champions/details.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
 export default Route.extend({
+    champion: null,
+
+    model(params) {
+        fetch(`http://localhost:3001/champions/${params.id}`).then(response => {
+            response.json().then(champion => {
+                this.set('champion', champion);
+                console.log(champion);
+                return {
+                    champion
+                }
+            })
+        });
+    }
 });
